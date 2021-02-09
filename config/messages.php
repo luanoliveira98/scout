d287e140981267a23f3687c9b652ba2347bc81af
Luan Oliveira <luanoliveira17@hotmail.com>
2021-02-09 20:10:15 -0300

    Criando configuracao de messages

---
+++ config/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @category    Messages-service
+ * @package     Messages
+ * @author      Luan Oliveira <luanoliveira17@hotmail.com>
+ */
+
+return [
+    'index' => [
+        'success' => 'Dados listados com sucesso'
+    ]
+];
