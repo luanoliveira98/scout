56eb15f0917b702ec698bbf6641331bd3581294c
Luan Oliveira <luan.oliveira@uhuu.com>
2021-02-09 16:57:19 -0400

    Rotas de equipe

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+// EQUIPES
+$router->group(['prefix' => 'equipes'], function() use ($router) {
+    $router->get('/',           'EquipeController@index');
+    $router->post('/',          'EquipeController@store');
+    $router->get('/{id}',       'EquipeController@show');
+    $router->put('/{id}',       'EquipeController@update');
+    $router->delete('/{id}',    'EquipeController@destroy');
 });
